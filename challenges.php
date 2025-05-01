0699250e84a5d6257a50342b87b70313e146d682
RaselMridha792 <raselmridha792@gmail.com>
2025-05-01 16:34:10 +0600

    compare 3 numbers using function and if-else statement

--- challenges.php
+++ challenges.php
@@ -11,11 +11,11 @@ function even_Odd_checker($a){
             echo("your entered number is odd");
       }
 }
-
 // even_Odd_checker(6);
 
-// Problem 2: Simple Calculator
 
+
+// Problem 2: Simple Calculator
 function calculator($a, $b, $operation){
 
       if($operation == '+'){
@@ -30,8 +30,7 @@ function calculator($a, $b, $operation){
             echo("wrong operation can't perform");
       }
 }
-
-calculator(10, 20, '+');
+// calculator(10, 20, '+');
 
 function calculator_SC($a, $b, $operator){
       switch($operator){
@@ -51,8 +50,24 @@ function calculator_SC($a, $b, $operator){
             echo("wrong operation can't perform");
       }
 }
+// calculator_SC(10, 20, '+');
 
-calculator_SC(10, 20, '+');
+
+// Problem 3: Find Maximum Between Three Numbers
+
+function maxNum($a, $b, $c){
+            if(($a > $b) && ($a > $c)){
+                  echo("the big max number is: " . $a);
+            }elseif(($b > $c) && ($b > $a)){
+                  echo("the big max number is: " . $b);
+            }elseif( ($c > $a) && ($c > $b)){
+                  echo("the big max number is: " . $c);
+            }else{
+                  echo("invalid input");     
+            }
+                  
+}
+maxNum(2, 3, 51);
 
 
 ?>
