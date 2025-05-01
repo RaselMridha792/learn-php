7fca9fd375a394798bf04c1b30c402895b19e211
RaselMridha792 <raselmridha792@gmail.com>
2025-05-01 16:11:39 +0600

    create a function to make a simple calculator

--- challenges.php
+++ challenges.php
@@ -12,7 +12,31 @@ function even_Odd_checker($a){
       }
 }
 
-even_Odd_checker(6);
+// even_Odd_checker(6);
+
+// Problem 2: Simple Calculator
+
+function calculator($a, $b, $operation){
+
+      if($operation == '+'){
+            echo("your answer is: " . ($a + $b));
+      }elseif($operation == '-'){
+            echo("your answer is: " . ($a - $b));
+      }elseif($operation == '/'){
+            echo("your answer is: " . ($a / $b));
+      }elseif($operation == '*'){
+            echo("your answer is: " . ($a * $b));
+      }else{
+            echo("wrong operation can't perform");
+      }
+
+      // switch($operation){
+      //       catch $operation == '+':
+      //             echo("")
+      // }
+}
+
+calculator(10, 20, '+');
 
 
 ?>
