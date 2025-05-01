e1628fe8c6710c3c683b3c11c230147484cde534
RaselMridha792 <raselmridha792@gmail.com>
2025-05-01 16:00:50 +0600

    solve even or odd checker

---
+++ challenges.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+// Problem 1: Even or Odd Checker
+
+function even_Odd_checker($a){
+      if($a % 2 == 0){
+            echo("your entered number is even");
+      }else{
+            echo("your entered number is odd");
+      }
+}
+
+even_Odd_checker(6);
+
+
+?>
